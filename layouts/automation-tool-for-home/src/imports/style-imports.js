b2f91250d80bf37679ac47fb78be60eb70c0296f
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-03-26 20:03:42 +0300

    Header and What_we_do section have been added.

--- layouts/automation-tool-for-home/src/imports/style-imports.js
+++ layouts/automation-tool-for-home/src/imports/style-imports.js
@@ -1,5 +1,14 @@
 import '@/common.blocks/common.scss';
 
+import '@/common.blocks/body/__header/body__header.scss';
+import '@/common.blocks/body/__services/body__services.scss';
+
+import '@/common.blocks/text/text.scss';
+import '@/common.blocks/text/_color/text_color_dark.scss';
+import '@/common.blocks/text/_color/text_color_light.scss';
+
+import '@/common.blocks/button/button.scss';
+
 import '@/common.blocks/nav/nav.scss';
 import '@/common.blocks/nav/__button/nav__button.scss';
 import '@/common.blocks/nav/__button-wrapper/nav__button-wrapper.scss';
@@ -7,10 +16,27 @@ import '@/common.blocks/nav/__inner/nav__inner.scss';
 import '@/common.blocks/nav/__logo/nav__logo.scss';
 
 import '@/common.blocks/header/header.scss';
+import '@/common.blocks/header/__inner/header__inner.scss';
 import '@/common.blocks/header/__button/header__button.scss';
 import '@/common.blocks/header/__content-wrapper/header__content-wrapper.scss';
 import '@/common.blocks/header/__image/header__image.scss';
 import '@/common.blocks/header/__text/header__text.scss';
 import '@/common.blocks/header/__title/header__title.scss';
 
-import '@/common.blocks/header/header.scss';
+import '@/common.blocks/section-title/section-title.scss';
+
+import '@/common.blocks/services/services.scss';
+import '@/common.blocks/services/__inner/services__inner.scss';
+import '@/common.blocks/services/__title/services__title.scss';
+import '@/common.blocks/services/__card/services__card.scss';
+
+import '@/common.blocks/service-card/service-card.scss';
+import '@/common.blocks/service-card/__image-wrapper/service-card__image-wrapper.scss';
+import '@/common.blocks/service-card/__image/service-card__image.scss';
+import '@/common.blocks/service-card/__image/_indent-right/service-card__image_indent-right_big.scss';
+import '@/common.blocks/service-card/__image/_indent-right/service-card__image_indent-right_medium.scss';
+import '@/common.blocks/service-card/__inner/service-card__inner.scss';
+import '@/common.blocks/service-card/__title/service-card__title.scss';
+import '@/common.blocks/service-card/__text/service-card__text.scss';
+import '@/common.blocks/service-card/__link/service-card__link.scss';
+import '@/common.blocks/service-card/__icon/service-card__icon.scss';
