912158967be6294f682f3080e1e67df882a63037
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-03-28 01:41:09 +0300

    Project section has been added

--- layouts/automation-tool-for-home/src/imports/style-imports.js
+++ layouts/automation-tool-for-home/src/imports/style-imports.js
@@ -6,6 +6,7 @@ import '@/common.blocks/body/__services/body__services.scss';
 import '@/common.blocks/text/text.scss';
 import '@/common.blocks/text/_color/text_color_dark.scss';
 import '@/common.blocks/text/_color/text_color_light.scss';
+import '@/common.blocks/text/_align/text_align_right.scss';
 
 import '@/common.blocks/button/button.scss';
 
@@ -26,6 +27,8 @@ import '@/common.blocks/header/__text/header__text.scss';
 import '@/common.blocks/header/__title/header__title.scss';
 
 import '@/common.blocks/section-title/section-title.scss';
+import '@/common.blocks/section-title/_color/section-title_color_white.scss';
+import '@/common.blocks/section-title/_color/section-title_color_dark.scss';
 
 import '@/common.blocks/services/services.scss';
 import '@/common.blocks/services/__inner/services__inner.scss';
@@ -42,3 +45,30 @@ import '@/common.blocks/service-card/__title/service-card__title.scss';
 import '@/common.blocks/service-card/__text/service-card__text.scss';
 import '@/common.blocks/service-card/__link/service-card__link.scss';
 import '@/common.blocks/service-card/__icon/service-card__icon.scss';
+
+import '@/common.blocks/projects/projects.scss';
+import '@/common.blocks/projects/__cards-wrapper/projects__cards-wrapper.scss';
+import '@/common.blocks/projects/__card/projects__card.scss';
+import '@/common.blocks/projects/__inner/projects__inner.scss';
+import '@/common.blocks/projects/__title/projects__title.scss';
+import '@/common.blocks/projects/__text/projects__text.scss';
+
+import '@/common.blocks/project-card/project-card.scss';
+import '@/common.blocks/project-card/_width/project-card_width_short.scss';
+import '@/common.blocks/project-card/_text-align/project-card_text-align_right.scss';
+import '@/common.blocks/project-card/_img/project-card_img_1.scss';
+import '@/common.blocks/project-card/_img/project-card_img_2.scss';
+import '@/common.blocks/project-card/_img/project-card_img_3.scss';
+import '@/common.blocks/project-card/__image/project-card__image.scss';
+import '@/common.blocks/project-card/__overlay/project-card__overlay.scss';
+import '@/common.blocks/project-card/__overlay/_gradient/project-card__overlay_gradient_1.scss';
+import '@/common.blocks/project-card/__overlay/_gradient/project-card__overlay_gradient_2.scss';
+import '@/common.blocks/project-card/__overlay/_gradient/project-card__overlay_gradient_3.scss';
+import '@/common.blocks/project-card/__content-wrapper/project-card__content-wrapper.scss';
+import '@/common.blocks/project-card/__text/project-card__text.scss';
+import '@/common.blocks/project-card/__title/project-card__title.scss';
+import '@/common.blocks/project-card/__title/_text-align/project-card__title_text-align_right.scss';
+import '@/common.blocks/project-card/__title/_mt/project-card__title_mt_m.scss';
+import '@/common.blocks/project-card/__title/_mt/project-card__title_mt_s.scss';
+
+import '@/styles/selectors-chain/set-overlay-opacity-on-hover.scss';
