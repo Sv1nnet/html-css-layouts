5546166fa7360c25d1c1a0a7f111591cf92f5339
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-04-11 15:19:24 +0300

    header__image position and related elements styles changed

--- layouts/automation-tool-for-home/src/imports/script-imports.js
+++ layouts/automation-tool-for-home/src/imports/script-imports.js
@@ -2,3 +2,5 @@ import '@/common.blocks/header/__button/header__button.js';
 import '@/common.blocks/service-card/service-card.js';
 import '@/common.blocks/project-card/project-card.js';
 import '@/common.blocks/clients/__logo/clients__logo.js';
+import '@/common.blocks/nav/__button/nav__button.js';
+import '@/common.blocks/nav/__menu/nav__menu.js';
