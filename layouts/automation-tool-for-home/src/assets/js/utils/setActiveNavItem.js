0eed77a846f184105e2111cdcfdc9f58241422e9
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-04-13 02:50:32 +0300

    Update setActiveNavItem.js

--- layouts/automation-tool-for-home/src/assets/js/utils/setActiveNavItem.js
+++ layouts/automation-tool-for-home/src/assets/js/utils/setActiveNavItem.js
@@ -3,33 +3,41 @@ import switchActiveNavItem from '@blocks/nav-menu/__list-item/nav-menu__list-ite
 import throttle from '@assets/js/utils/throttle';
 
 const $ = window.$;
+// We need this padding because on mobile (Android Mozila and Chrome) we can't scroll
+// a page to the very bottom, so we need a little padding for checking the bottom of the page
+const MOBILE_BOTTOM_PADDDING = 100;
 
 function setActiveNavItem({ data }) {
-  const scrollPos = $(document).scrollTop();
-  let closestElement;
-  
-  if (scrollPos === 0) {
-    closestElement = data.$navItems[0];
-  }
-
   throttle(() => {
+    let closestElement;
 
     const { $navItems, $sections } = data;
     const scrollPos = $(document).scrollTop();
+    const isPageFullyScrolled = $(window).scrollTop() + $(window).height() >= $(document).height() - MOBILE_BOTTOM_PADDDING;
+  
+    if (scrollPos === 0) {
+      closestElement = $navItems[0];
+    } else if (isPageFullyScrolled) {
+      closestElement = $navItems[$navItems.length - 1];
+    }
 
-    Array.prototype.some.call($sections, function(el) {
-      const { top } = $(el).offset();
-      const height = $(el).height();
-      
-      if (top <= scrollPos && top + height > scrollPos) {
-        closestElement = $navItems.filter((_, listItem) => $(listItem).data('scrollTo') == $(el).attr('id'));
-        return true;
-      }
+    if (!closestElement) {
+      Array.prototype.some.call($sections, function(el) {
+        const { top } = $(el).offset();
+        const height = $(el).height();
 
-      return false;
-    });
+        if (top <= scrollPos && top + height > scrollPos) {
+          closestElement = $navItems.filter((_, listItem) => $(listItem).data('scrollTo') == $(el).attr('id'))[0];
+          return true;
+        }
 
-    if (closestElement) switchActiveNavItem($navItems, closestElement[0]);
+        return false;
+      });
+    }
+
+    if (closestElement) {
+      switchActiveNavItem($navItems, closestElement);
+    }
   }, 150);
 }
 
