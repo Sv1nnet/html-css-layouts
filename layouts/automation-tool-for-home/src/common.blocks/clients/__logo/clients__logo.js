6df5c2880b279c49f1d124fbe331f580cf5db832
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-04-12 19:34:01 +0300

    Transition delay adjusted

--- layouts/automation-tool-for-home/src/common.blocks/clients/__logo/clients__logo.js
+++ layouts/automation-tool-for-home/src/common.blocks/clients/__logo/clients__logo.js
@@ -4,16 +4,16 @@ const $ = window.$;
 const $cards = $('.clients__logo');
 const isDesktop = window.innerWidth > 768;
 
-let delay = 0.6;
+let delay = 0;
 /* Every time the window is scrolled ... */
 if (isDesktop) {
   $cards.each(function() {
     $(this).css({
-      transition: `${delay}s`,
+      transition: `.6s ${delay}s`,
       opacity: '0',
     });
 
-    delay += 0.5;
+    delay += 0.2;
   });
 
   fadeOutElements($cards);
