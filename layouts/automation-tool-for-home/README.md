d4a782d39babb8be8e86398e771b700f01eda655
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-04-13 14:41:12 +0300

    const IP has been added.

--- layouts/automation-tool-for-home/README.md
+++ layouts/automation-tool-for-home/README.md
@@ -1,1 +1,1 @@
-Here I use BEM naming and jQuery.
+Here I use BEM naming and jQuery. To run it locally set your IP in index.js
--- layouts/automation-tool-for-home/src/index.js
+++ layouts/automation-tool-for-home/src/index.js
@@ -1,9 +1,11 @@
 import './imports/style-imports.js';
 
 window.onload = function() {
+  const IP = 'http://192.168.0.10:4242';
+
   function loadScript(url, options, callback) {
-    var script = document.createElement("script")
-    script.type = "text/javascript";
+    var script = document.createElement('script')
+    script.type = 'text/javascript';
 
     if (typeof options !== 'function') {
       for (const opt in options) {
@@ -16,7 +18,7 @@ window.onload = function() {
     if (script.readyState) {
       // handle IE
       script.onreadystatechange = function() {
-        if (script.readyState == "loaded" || script.readyState == "complete") {
+        if (script.readyState == 'loaded' || script.readyState == 'complete') {
           script.onreadystatechange = null;
           if (callback) callback();
         }
@@ -29,7 +31,7 @@ window.onload = function() {
     }
 
     script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName('head')[0].appendChild(script);
   }
 
   loadScript('https://code.jquery.com/jquery-3.4.1.min.js',
@@ -38,9 +40,9 @@ window.onload = function() {
     },
     function() {
       loadScript(
-        'http://192.168.0.10:4242/assets/js/libs/jQueryUI/jquery-ui.min.js',
+        `${IP}/assets/js/libs/jQueryUI/jquery-ui.min.js`,
         function() {
-          loadScript('http://192.168.0.10:4242/script-imports.js');
+          loadScript(`${IP}/script-imports.js`);
         });
     },
   );
